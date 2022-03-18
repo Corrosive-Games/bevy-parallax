f76c5bfc76f59ed886d228f67cab2379e68a7f21
Carlo Supina <cdsupina@gmail.com>
2022-03-18 01:04:59 -0500

    add scaled scrolling to background layers

--- examples/cyberpunk.rs
+++ examples/cyberpunk.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
-use bevy_parallax::{LayerData, ParallaxPlugin, ParallaxResource};
+use bevy_parallax::{LayerData, ParallaxMoveEvent, ParallaxPlugin, ParallaxResource};
 
 fn main() {
     let window = WindowDescriptor {
@@ -14,42 +14,61 @@ fn main() {
 
     App::new()
         .insert_resource(window)
-        .insert_resource(ParallaxResource::new(vec![
-            LayerData {
-                speed: 1.0,
-                path: "back.png".to_string(),
-                tile_size: Vec2::new(96.0, 160.0),
-                cols: 1,
-                rows: 1,
-                scale: 4.5,
-                z: 0.0,
-            },
-            LayerData {
-                speed: 1.0,
-                path: "middle.png".to_string(),
-                tile_size: Vec2::new(144.0, 160.0),
-                cols: 1,
-                rows: 1,
-                scale: 4.5,
-                z: 1.0,
-            },
-            LayerData {
-                speed: 1.0,
-                path: "front.png".to_string(),
-                tile_size: Vec2::new(272.0, 160.0),
-                cols: 1,
-                rows: 1,
-                scale: 4.5,
-                z: 2.0,
-            },
-        ]))
+        .insert_resource(ParallaxResource {
+            layer_data: vec![
+                LayerData {
+                    speed: 0.9,
+                    path: "back.png".to_string(),
+                    tile_size: Vec2::new(96.0, 160.0),
+                    cols: 1,
+                    rows: 1,
+                    scale: 4.5,
+                    z: 0.0,
+                },
+                LayerData {
+                    speed: 0.6,
+                    path: "middle.png".to_string(),
+                    tile_size: Vec2::new(144.0, 160.0),
+                    cols: 1,
+                    rows: 1,
+                    scale: 4.5,
+                    z: 1.0,
+                },
+                LayerData {
+                    speed: 0.1,
+                    path: "front.png".to_string(),
+                    tile_size: Vec2::new(272.0, 160.0),
+                    cols: 1,
+                    rows: 1,
+                    scale: 4.5,
+                    z: 2.0,
+                },
+            ],
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(ParallaxPlugin)
         .add_startup_system(initialize_camera_system)
+        .add_system(move_camera_system)
         .run();
 }
 
 pub fn initialize_camera_system(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+pub fn move_camera_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut move_event_writer: EventWriter<ParallaxMoveEvent>,
+) {
+    if keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right) {
+        move_event_writer.send(ParallaxMoveEvent {
+            camera_move_speed: 2.0,
+        });
+    } else if keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left) {
+        move_event_writer.send(ParallaxMoveEvent {
+            camera_move_speed: -2.0,
+        });
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::view::Layer};
 
 /// Resource for managing parallax
+#[derive(Debug, Default)]
 pub struct ParallaxResource {
     pub layer_data: Vec<LayerData>,
-    layer_entities: Vec<Entity>,
+    pub layer_entities: Vec<Entity>,
+    pub window_size: Vec2,
+    pub camera_name: Option<String>,
 }
 
 impl ParallaxResource {
@@ -12,13 +15,15 @@ impl ParallaxResource {
         ParallaxResource {
             layer_data,
             layer_entities: vec![],
+            window_size: Vec2::ZERO,
+            camera_name: None,
         }
     }
 
     /// Delete all layer entities in parallax resource and empty Vec
     pub fn despawn_layers(&mut self, commands: &mut Commands) {
         for entity in self.layer_entities.iter() {
-            commands.entity(*entity).despawn();
+            commands.entity(*entity).despawn_recursive();
         }
 
         self.layer_entities = vec![];
@@ -40,24 +45,53 @@ impl ParallaxResource {
             let texture_atlas =
                 TextureAtlas::from_grid(texture_handle, layer.tile_size, layer.cols, layer.rows);
             let texture_atlas_handle = texture_atlases.add(texture_atlas);
-            commands
-                .spawn()
+            let spritesheet_bundle = SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle,
+                ..Default::default()
+            };
+            let mut entity_commands = commands.spawn();
+            entity_commands
                 .insert(LayerComponent { speed: layer.speed })
-                .insert(Name::new(format!("Parallax ({})", i)))
-                .insert_bundle(SpriteSheetBundle {
-                    texture_atlas: texture_atlas_handle,
-                    transform: Transform {
-                        translation: Vec3::new(0.0, 0.0, layer.z),
-                        scale: Vec3::new(layer.scale, layer.scale, 1.0),
-                        ..Default::default()
-                    },
+                .insert(Name::new(format!("Parallax Layer ({})", i)))
+                .insert(Transform {
+                    translation: Vec3::new(0.0, 0.0, layer.z),
+                    scale: Vec3::new(layer.scale, layer.scale, 1.0),
                     ..Default::default()
+                })
+                .insert(GlobalTransform::default())
+                .with_children(|parent| {
+                    parent
+                        .spawn_bundle(spritesheet_bundle.clone())
+                        .insert(LayerTextureComponent);
+
+                    let mut max_x = (layer.tile_size.x / 2.0) * layer.scale;
+
+                    let mut adjusted_spritesheet_bundle = spritesheet_bundle.clone();
+
+                    // spawn 2 windows length of background textures
+                    while max_x < self.window_size.x {
+                        adjusted_spritesheet_bundle.transform.translation.x += layer.tile_size.x;
+                        max_x += layer.tile_size.x * layer.scale;
+                        parent
+                            .spawn_bundle(adjusted_spritesheet_bundle.clone())
+                            .insert(LayerTextureComponent);
+
+                        parent
+                            .spawn_bundle({
+                                let mut bundle = adjusted_spritesheet_bundle.clone();
+                                bundle.transform.translation.x *= -1.0;
+                                bundle
+                            })
+                            .insert(LayerTextureComponent);
+                    }
                 });
+            self.layer_entities.push(entity_commands.id());
         }
     }
 }
 
 /// Layer initialization data
+#[derive(Debug)]
 pub struct LayerData {
     pub speed: f32,
     pub path: String,
@@ -70,13 +104,18 @@ pub struct LayerData {
 
 #[derive(Component)]
 pub struct LayerComponent {
-    speed: f32,
+    speed: f32, // speed of layer relative to camera
 }
+
+#[derive(Component)]
+pub struct LayerTextureComponent;
 
 pub struct ParallaxPlugin;
 impl Plugin for ParallaxPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(initialize_parallax_system);
+        app.add_event::<ParallaxMoveEvent>()
+            .add_startup_system(initialize_parallax_system)
+            .add_system(follow_camera_system);
     }
 }
 
@@ -84,7 +123,56 @@ fn initialize_parallax_system(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    windows: Res<Windows>,
     mut parallax_res: ResMut<ParallaxResource>,
 ) {
+    let window = windows.get_primary().unwrap();
+    parallax_res.window_size = Vec2::new(window.width(), window.height());
     parallax_res.create_layers(&mut commands, &asset_server, &mut texture_atlases);
+
+    // TODO: Remove
+    println!("{:?}", parallax_res);
 }
+
+pub struct ParallaxMoveEvent {
+    pub camera_move_speed: f32,
+}
+
+fn follow_camera_system(
+    mut camera_query: Query<(&mut Transform, &Camera)>,
+    mut layer_query: Query<(&mut Transform, &LayerComponent), Without<Camera>>,
+    parallax_resource: Res<ParallaxResource>,
+    mut move_events: EventReader<ParallaxMoveEvent>,
+) {
+    for (mut camera_transform, camera) in camera_query.iter_mut() {
+        if (parallax_resource.camera_name.is_some()
+            && parallax_resource.camera_name.as_ref().unwrap() == camera.name.as_ref().unwrap())
+            || (parallax_resource.camera_name.is_none()
+                && camera.name.as_ref().unwrap() == "camera_2d")
+        {
+            for event in move_events.iter() {
+                camera_transform.translation.x += event.camera_move_speed;
+                for (mut layer_transform, layer) in layer_query.iter_mut() {
+                    layer_transform.translation.x += event.camera_move_speed * layer.speed;
+                }
+            }
+
+            break;
+        } else {
+            println!("camera not found")
+        }
+    }
+}
+
+/*
+fn update_layer_textures(
+    layer_query: Query<(&Transform, &Children), With<LayerComponent>>,
+    mut texture_query: Query<&Transform, With<LayerTextureComponent>>,
+) {
+    for (layer_transform, children) in layer_query.iter() {
+        for &child in children.iter() {
+            let texture_transform = texture_query.get(child);
+        }
+    }
+}
+*/
